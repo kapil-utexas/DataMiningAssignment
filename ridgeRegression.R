d858507b404c9b1ca8165f6c2063a8412a44a0ae
kapil <kapil@utexas.edu>
2015-11-13 19:22:41 -0600

    full dataset run

--- ridgeRegression.R
+++ ridgeRegression.R
@@ -11,7 +11,7 @@ data$SchoolHoliday = as.numeric(factor(data$SchoolHoliday))
 data$Date = as.numeric(factor(data$Date))
 #Z score normlization
 normData = scale(data, center = TRUE, scale = TRUE)
-sdVal = apply(normData,2,sd)
+#sdVal = apply(normData,2,sd)
 #linearRidge(formula, data, lambda = "automatic", nPCs = NULL,scaling = c("corrForm", "scale", "none"), ...)
 
 pca = princomp(normData[,-4])
@@ -23,8 +23,12 @@ screeplot(pca,type="lines")
 
 #qplot(pca$eig, type="lines")
 
-Y = newData[1:500,4]
-dat = cbind(data.frame(Y),as.data.frame(newData[1:500,]) )
+Y = normData[,4]
+dat = cbind(data.frame(Y),as.data.frame(newData) )
+#scale(dat,center = TRUE, scale = TRUE)
+#sdVal = apply(dat,2,sd)
+#print(sdVal)
+
 model = linearRidge( Y  ~ .  ,data =  dat, nPCs = 6 ) 
 summary(model)                     
 
