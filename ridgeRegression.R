687d0eedef9be980c63bb0fabf7bcc8cd65bc6d3
kapil <kapil@utexas.edu>
2015-11-13 18:53:00 -0600

    linear ridge method works now

--- ridgeRegression.R
+++ ridgeRegression.R
@@ -1,33 +1,32 @@
 #library(recommenderlab)
 library(ridge)
+#library(MASS)
+#library(FactoMineR)
+library(ggplot2)
+        
 
 data = read.csv('train.csv',header=TRUE,sep=",")
 data$StateHoliday = as.numeric(factor(data$StateHoliday))
 data$SchoolHoliday = as.numeric(factor(data$SchoolHoliday))
-#data$Date = as.numeric((as.POSIXct(factor(data$Date), format="%m/%d/20%Y")))
-#Date = (as.POSIXct(data$Date, format="%m/%d/%y"))
-#print(Date)
 data$Date = as.numeric(factor(data$Date))
-#print(Date) 
 #Z score normlization
 normData = scale(data, center = TRUE, scale = TRUE)
 sdVal = apply(normData,2,sd)
-#meanVal = apply(normData,2,mean)
-print(sdVal)
-#print (meanVal)
 #linearRidge(formula, data, lambda = "automatic", nPCs = NULL,scaling = c("corrForm", "scale", "none"), ...)
 
-pcaData = prcomp(normData[,-4],
-                 center = TRUE,
-                 scale. = TRUE)
-print(pcaData)
+pca = princomp(normData[,-4])
+#summary(pca)
+#print(pca$loadings)
+newData = (pca$scores)
+screeplot(pca,type="lines")
 
 
+#qplot(pca$eig, type="lines")
 
-
-dat = cbind(data.frame(Y=normData[1:5,4]),as.data.frame(normData[1:5,-4]))
-model = linearRidge( Y  ~ .  ,data =  dat , nPCs = 8) 
-                     
+Y = newData[1:500,4]
+dat = cbind(data.frame(Y),as.data.frame(newData[1:500,]) )
+model = linearRidge( Y  ~ .  ,data =  dat, nPCs = 6 ) 
+summary(model)                     
 
 #summ = summary(model)
 #print(summ)
